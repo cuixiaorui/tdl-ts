649db64d61d5b588f1823251e311b7ae98335f3d
cuixiaorui <cui_xiaorui@126.com>
2022-01-27 18:25:47 +0800

    feat: add easy-tuple-length

--- type-challenges/18-easy-tuple-length/template.ts
+++ type-challenges/18-easy-tuple-length/template.ts
@@ -1,1 +1,11 @@
-type Length<T  extends readonly any[]> = T["length"];
+type Length<T extends readonly any[]> = T["length"];
+
+// js
+function getLength(arr) {
+  if (!Array.isArray(arr)) return;
+  return arr.length;
+}
+
+// 知识点
+// 什么是 tuple 类型
+// tuple 和普通的数组有什么区别
--- type-challenges/18-easy-tuple-length/test-cases.ts
+++ type-challenges/18-easy-tuple-length/test-cases.ts
@@ -1,7 +1,17 @@
-import { Equal, Expect } from '@type-challenges/utils'
+import { Equal, Expect } from "@type-challenges/utils";
 
-const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
-const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+const tesla = ["tesla", "model 3", "model X", "model Y"]  as const
+
+
+type t55 = typeof tesla
+
+const spaceX = [
+  "FALCON 9",
+  "FALCON HEAVY",
+  "DRAGON",
+  "STARSHIP",
+  "HUMAN SPACEFLIGHT",
+] as const;
 
 type cases = [
   Expect<Equal<Length<typeof tesla>, 4>>,
@@ -9,5 +19,15 @@ type cases = [
   // @ts-expect-error
   Length<5>,
   // @ts-expect-error
-  Length<'hello world'>,
-]
+  Length<"hello world">
+];
+
+type StringNumberPair = [string, number];
+
+
+type stringArr = string[]
+type t3 = stringArr["length"]
+
+type t2 = StringNumberPair["length"]
+
+const str: StringNumberPair = ["123", 123];

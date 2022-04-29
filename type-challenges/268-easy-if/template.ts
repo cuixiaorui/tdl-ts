7eb3f6e00bbe086424579d73509b2f7921bbd8de
cuixiaorui <cui_xiaorui@126.com>
2022-04-29 21:11:30 +0800

    feat: add js logic

--- type-challenges/268-easy-if/template.ts
+++ type-challenges/268-easy-if/template.ts
@@ -1,6 +1,11 @@
-type If<C extends boolean, T, F> = C extends true ? T : F
+type If<C extends boolean, T, F> = C extends true ? T : F;
 
+// js
+function If(C: any, T: any, F: any) {
+  return C ? T : F;
+}
 
 // 知识点
+// 类型兼容性 | 分配规则
 // null 严格模式和非严格模式的区别
 // 1. https://www.typescriptlang.org/docs/handbook/type-compatibility.html

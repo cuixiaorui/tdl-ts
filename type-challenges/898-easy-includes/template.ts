23b03b1857e65e7afccb0b87ae0ce82d943cf082
cuixiaorui <cui_xiaorui@126.com>
2022-05-12 20:46:58 +0800

    feat: add recursive logic for includes

--- type-challenges/898-easy-includes/template.ts
+++ type-challenges/898-easy-includes/template.ts
@@ -1,25 +1,45 @@
 import { Equal } from "@type-challenges/utils";
 
-export type Includes<T extends any[], U> = T extends [infer first, ...infer Rest]
-  ? Equal<first, U> extends true
+export type Includes<T extends any[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Equal<First, U> extends true
     ? true
     : Includes<Rest, U>
   : false;
 
 // js
-
+// for 循环的实现
 // function Includes(list, key) {
 //   for (let i = 0; i < list.length; i++) {
-//     const item = list[i];
-//     if (item === key) {
+//     const element = list[i];
+//     if (element === key) {
 //       return true;
 //     }
 //   }
-//   return false;
+
+//   return false
 // }
 
+// 递归的实现
+function Includes(list, key) {
+  function _(list, key) {
+    if (list.length === 0) return false;
+    const [first, ...rest] = list;
+
+    if (first === key) {
+      return true;
+    } else {
+      return _(rest, key);
+    }
+  }
+
+  return _(list, key);
+}
+
 // 知识点
-// 1. 如何遍历数组(一个值一个值的取出来)
-// 2. ts 的模块规范  
-//    - 如果有 export 的话，那么就是模块
+// 1. 用递归实现遍历数组
+// 2. ts 的模块规范
+//    - 如果有 export/import 的话，那么就是模块
 //    - 没有的话，那么就是全局的 ，可以直接在别的模块引用
